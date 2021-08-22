5356bccded5b7d90a0728f934c761514512ea8d6
Raj Ranjan <rranjan01234@gmail.com>
2021-08-22 17:16:47 +0530

    Update truffle-config.js

--- src/truffle-config.js
+++ src/truffle-config.js
@@ -1,44 +1,33 @@
-const Web3 = require('web3');
-const ethers = require('ethers');
-const ContractKit = require('@celo/contractkit');
-require('dotenv').config();
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const RPCURL = `https://alfajores-forno.celo-testnet.org`;
-const web3 = new Web3(RPCURL);
-const kit = ContractKit.newKitFromWeb3(web3);
-
-async function awaitWrapper(){
-  let account = ethers.Wallet.fromMnemonic(process.env.MNEMONIC, "m/44'/52752'/0'/0/0")
-  let celoToken = await kit.contracts.getGoldToken();
-
-  let celoBalance = await celoToken.balanceOf(account.address);
-  console.log("Account address: ", account.address);
-  console.log(`CELO Balance: ${celoBalance / (10**18)}`);
-  
-  if(celoBalance / (10**18) < 0.4) {
-    console.log("Balance too low to deploy contracts. Please fund your account here at https://celo.org/developers/faucet\n");
-  }
-  kit.connection.addAccount(account.privateKey)
-}
-
-awaitWrapper();
+// Account credentials from which our contract will be deployed
+const MNEMONIC = process.env.MNEMONIC;
 
 module.exports = {
+  contracts_build_directory: "./src/build/contracts",
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*"
+      network_id: "*",
     },
-    alfajores: {
-      provider: kit.connection.web3.currentProvider,
-      network_id: 44787
-    }
+    fuji: {
+      provider: function () {
+        return new HDWalletProvider({
+          mnemonic: MNEMONIC,
+          providerOrUrl: `https://api.avax-test.network/ext/bc/C/rpc`,
+        });
+      },
+      network_id: "*",
+      gas: 3000000,
+      gasPrice: 470000000000,
+      skipDryRun: true,
+    },
   },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
+  compilers: {
+    solc: {
+      version: "0.8.0"
     }
   }
-}
+};
